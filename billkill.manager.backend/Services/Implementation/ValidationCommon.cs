1d0ba3299ff20caa3261feaca0a42544530b2651
Admin <Admin@DESKTOP-34CAHMM>
2023-08-24 15:08:18 +0400

    change error codes

--- billkill.manager.backend/Services/Implementation/ValidationCommon.cs
+++ billkill.manager.backend/Services/Implementation/ValidationCommon.cs
@@ -19,25 +19,12 @@ namespace billkill.manager.backend.Services.Implementation
                 case ErrorCodes.NOT_FOUND:
                     return StatusCodeModel.NOT_FOUND;
 
-                case ErrorCodes.LOOKUP:
                 case ErrorCodes.REQUIRED:
                 case ErrorCodes.FORMAT:
                     return StatusCodeModel.BAD_REQUEST;
 
-                case ErrorCodes.AVIS_NOT_FOUND:
-                case ErrorCodes.IAMAS_NOT_FOUND:
-                case ErrorCodes.IAMAS_DOC_PASSIVE:
-                case ErrorCodes.CUSTOMER:
-                case ErrorCodes.RELATED_PEOPLE:
-                case ErrorCodes.OPERATION:
-                    return StatusCodeModel.OK;
-
-                case ErrorCodes.IAMAS_SERVER:
-                case ErrorCodes.AVIS_SERVER:
                 case ErrorCodes.SYSTEM:
                 case ErrorCodes.DB:
-                case ErrorCodes.MODEL_STATE:
-                case ErrorCodes.BULK:
                     return StatusCodeModel.INTERNEL_SERVER;
             }
             return StatusCodeModel.OK;
