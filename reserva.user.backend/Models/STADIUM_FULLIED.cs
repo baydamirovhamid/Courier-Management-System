1beeccf0dfef4a1553e00920258cfb09d5d2c32d
shixismayil <sixismayil@gmail.com>
2023-10-17 19:58:30 +0400

    model changes

--- reserva.user.backend/Models/STADIUM_FULLIED.cs
+++ reserva.user.backend/Models/STADIUM_FULLIED.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace reserva.user.backend.Models;
 
 public partial class StadiumFullied
-{
+{    
+    [Column("id")]
     public int Id { get; set; }
 
+    [Column("stadium_id")]
     public int? StadiumId { get; set; }
 
+    [Column("start_time")]
     public int? StartTime { get; set; }
 
+    [Column("end_time")]
     public int? EndTime { get; set; }
 
+    [Column("created_at")]
     public DateOnly? CreatedAt { get; set; }
 
+    [Column("created_by")]
     public string? CreatedBy { get; set; }
 
+    [Column("updated_at")]
     public DateOnly? UpdatedAt { get; set; }
 
+    [Column("updated_by")]
     public string? UpdatedBy { get; set; }
 
+    [Column("stadium")]
     public virtual Stadium? Stadium { get; set; }
 }
--- reserva.user.backend/Models/STADIUM_PRICE.cs
+++ reserva.user.backend/Models/STADIUM_PRICE.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace reserva.user.backend.Models;
 
 public partial class StadiumPrice
 {
+    [Column("id")]
     public int Id { get; set; }
 
+    [Column("price")]
     public int? Price { get; set; }
 
+    [Column("time_type_id")]
     public int? TimeTypeId { get; set; }
 
+    [Column("stadium_id")]
     public int? StadiumId { get; set; }
 
+    [Column("stadium")]
     public virtual Stadium? Stadium { get; set; }
 
+    [Column("time_type")]
     public virtual TIME_TYPE? TimeType { get; set; }
 }
