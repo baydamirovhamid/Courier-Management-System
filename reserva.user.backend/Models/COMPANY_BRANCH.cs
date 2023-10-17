0a4405cb7129cd06b3e7bd4db18b80b01c66e16d
Hajibayov <shacibyov@gmail.com>
2023-10-17 19:41:10 +0400

    employee and branch edited

---
+++ reserva.user.backend/Models/COMPANY_BRANCH.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reserva.user.backend.Models;
+
+public partial class COMPANY_BRANCH
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("phone")]
+    public string? Phone { get; set; }
+
+    [Column("address")]
+    public string? Address { get; set; }
+
+    [Column("company_id")]
+    public int? CompanyId { get; set; }
+
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Column("created_by")]
+    public string? CreatedBy { get; set; }
+
+    [Column("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [Column("updated_by")]
+    public string? UpdatedBy { get; set; }
+
+    public virtual COMPANY Company { get; set; }
+
+}
