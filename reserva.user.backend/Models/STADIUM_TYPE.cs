9d38a8eba2e308da0d07d67a4bcfa4d73c2ddc5f
Xadica Allahverdi <allahverdi.xadica@gmail.com>
2023-10-17 19:59:41 +0400

    model changes

---
+++ reserva.user.backend/Models/STADIUM_TYPE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace reserva.user.backend.Models;
+
+public partial class STADIUM_TYPE
+{
+    [Column("id"), Key]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("key")]
+    public string? Key { get; set; }
+}
