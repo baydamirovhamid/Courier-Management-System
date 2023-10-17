fdb026c1ecdb41359640ac7ed3de61c4b722e707
MuhammedHuseyn <salamovsalam1w@gmail.com>
2023-10-17 19:57:46 +0400

    change

--- reserva.user.backend/Models/StadiumType.cs
+++ reserva.user.backend/Models/StadiumType.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 
 namespace reserva.user.backend.Models;
 
-public partial class StadiumType
+public partial class STADIUMTYPE
 {
+    
     public int Id { get; set; }
-
+    [Column("id"), Key]
+    
     public string? Name { get; set; }
-
+    [Column("name")]
+   
     public string? Key { get; set; }
+     [Column("key")]
 
     public virtual ICollection<Stadium> Stadia { get; set; } = new List<Stadium>();
 }
