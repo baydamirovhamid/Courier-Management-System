7f7cfcc3767b1cd10db0c2c793195c2eafa7fb1c
baydamirov <baydamirovv35@gmail.com>
2023-10-17 19:52:57 +0400

    fcommit

--- reserva.user.backend/Models/Stadium.cs
+++ reserva.user.backend/Models/Stadium.cs
@@ -5,37 +5,43 @@ namespace reserva.user.backend.Models;
 
 public partial class Stadium
 {
+     [Column("id"), Key]
     public int Id { get; set; }
 
+     [Column("name"),]
     public string? Name { get; set; }
 
+     [Column("code"),]
     public string? Code { get; set; }
 
+     [Column("type_id"),]
     public int? TypeId { get; set; }
 
+     [Column("branc_id"),]
     public int? BranchId { get; set; }
 
+     [Column("has_recording"),]
     public int? HasRecording { get; set; }
 
+     [Column("min_price"),]
     public int? MinPrice { get; set; }
 
+     [Column("max_price"),]
     public int? MaxPrice { get; set; }
 
-    public DateOnly? CreatedAt { get; set; }
+     [Column("created_at"),]
+    public DateTime? CreatedAt { get; set; }
 
+     [Column("created_by"),]
     public string? CreatedBy { get; set; }
 
-    public DateOnly? UpdatedAt { get; set; }
+     [Column("updated_at"),]
+    public DateTime? UpdatedAt { get; set; }
 
+     [Column("updated_by"),]
     public string? UpdatedBy { get; set; }
 
     public virtual CompanyBranch? Branch { get; set; }
 
-    public virtual ICollection<Reserve> Reserves { get; set; } = new List<Reserve>();
-
-    public virtual ICollection<StadiumFullied> StadiumFullieds { get; set; } = new List<StadiumFullied>();
-
-    public virtual ICollection<StadiumPrice> StadiumPrices { get; set; } = new List<StadiumPrice>();
-
-    public virtual StadiumType? Type { get; set; }
+   
 }
