5c9c92f8a443ddf802d47195c724ddd2f6385f08
Xadica Allahverdi <allahverdi.xadica@gmail.com>
2023-11-04 10:33:29 +0400

    trigger

--- reserva.user.backend/Database/CreateScripts/Trigger Function.sql
+++ reserva.user.backend/Database/CreateScripts/Trigger Function.sql
@@ -1,55 +1,55 @@
 ﻿CREATE OR REPLACE FUNCTION STADIUM_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"stadium_seq"');
+   NEW."id":=NEXTVAL('"stadium_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION TIME_TYPE_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"time_type_seq"');
+   NEW."id":=NEXTVAL('"time_type_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION COMPANY_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"company_seq"');
+   NEW."id":=NEXTVAL('"company_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION COMPANY_BRANCH_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"company_branch_seq"');
+   NEW."id":=NEXTVAL('"company_branch_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION STADIUM_TYPE_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"stadium_type_seq"');
+   NEW."id":=NEXTVAL('"stadium_type_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION CLIENT_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"client_seq"');
+   NEW."id":=NEXTVAL('"client_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION STATIC_DATA_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"static_data_seq"');
+   NEW."id":=NEXTVAL('"static_data_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
 
  CREATE OR REPLACE FUNCTION RESERVE_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"reserve_seq"');
+   NEW."id":=NEXTVAL('"reserve_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
@@ -57,7 +57,7 @@
 
 CREATE OR REPLACE FUNCTION STADIUM_FULLIED_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"stadium_fullied_seq"');
+   NEW."id":=NEXTVAL('"stadium_fullied_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
@@ -65,7 +65,7 @@ CREATE OR REPLACE FUNCTION STADIUM_FULLIED_TG_FUNC() RETURNS "trigger" AS $BODY$
  
 CREATE OR REPLACE FUNCTION STADIUM_PRICE_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"stadium_price_seq"');
+   NEW."id":=NEXTVAL('"stadium_price_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
@@ -73,7 +73,7 @@ CREATE OR REPLACE FUNCTION STADIUM_PRICE_TG_FUNC() RETURNS "trigger" AS $BODY$
   
 CREATE OR REPLACE FUNCTION COMPANY_EMPLOYEE_TG_FUNC() RETURNS "trigger" AS $BODY$
  BEGIN
-   NEW."ID":=NEXTVAL('"company_employee_seq"');
+   NEW."id":=NEXTVAL('"company_employee_seq"');
    RETURN NEW;
  END;
  $BODY$ LANGUAGE 'plpgsql' VOLATILE;
