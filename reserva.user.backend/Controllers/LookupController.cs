4db1e0036b131d838e73fb52b3533fe5b4312112
Xadica Allahverdi <allahverdi.xadica@gmail.com>
2023-11-04 10:55:42 +0400

    test

--- reserva.user.backend/Controllers/LookupController.cs
+++ reserva.user.backend/Controllers/LookupController.cs
@@ -6,7 +6,6 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
 using System.Diagnostics;
 using reserva.user.backend.Services.Interface;
 using reserva.user.backend.DTO.ResponseModels.Inner;
-using reserva.user.backend.Models;
 
 
 namespace reserva.user.backend.Controllers
@@ -18,6 +17,27 @@ namespace reserva.user.backend.Controllers
         private readonly ILookupService _lookupService;
         public LookupController(ILookupService lookupService) {
             _lookupService = lookupService;
+        }
+
+        [HttpGet]
+        [Route("get-static-data")]
+        public async Task<IActionResult> GetStaticData(string key)
+        {
+            ResponseObject<StaticVM> response = new ResponseObject<StaticVM>();
+            response.Status = new StatusModel();
+            response.TraceID = Activity.Current?.Id ?? HttpContext?.TraceIdentifier;
+            try
+            {
+                response = await _lookupService.GetStaticDataAsync(response, key);
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+
+                response.Status.ErrorCode = ErrorCodes.SYSTEM;
+                response.Status.Message = "Sistemdə xəta baş verdi.";
+                return StatusCode(StatusCodeModel.INTERNEL_SERVER, response);
+            }
         }
 
         [HttpGet]
