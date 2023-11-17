5d43a83697d36c7b98e605a28babcec63fa402af
Hajibayov <shacibyov@gmail.com>
2023-11-17 13:32:36 +0400

    alter scripts added

--- reserva.user.backend/Database/AlterScripts/Table.sql
+++ reserva.user.backend/Database/AlterScripts/Table.sql
@@ -1,1 +1,5 @@
-﻿
+﻿ALTER TABLE stadium
+ALTER COLUMN has_recording TYPE BOOLEAN USING has_recording::boolean;
+
+ALTER TABLE reserve
+ALTER COLUMN is_deleted TYPE BOOLEAN USING is_deleted::boolean;
