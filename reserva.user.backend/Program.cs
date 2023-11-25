c80c593b68f12855aacb9767b8bd40e989797562
Ferhad <f.3.d.1.a@mail.ru>
2023-11-25 10:25:29 +0400

    StadiumPrice

--- reserva.user.backend/Program.cs
+++ reserva.user.backend/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddTransient<IStadiumService, StadiumService>();
 builder.Services.AddTransient<IValidationCommon, ValidationCommon>();
 builder.Services.AddTransient<IEmailService, EmailService>();
 builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<IStadiumPrice, StadiumPrice>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 
 builder.Services.AddDbContext<ReservaDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
