7e4b2defb495d649011d66ff8b2608cfabcab41c
Hajibayov <shacibyov@gmail.com>
2023-12-01 20:03:16 +0400

    update CompanyEmployee with put and patch

--- reserva.user.backend/Services/Interface/ICompanyEmployeeService.cs
+++ reserva.user.backend/Services/Interface/ICompanyEmployeeService.cs
@@ -1,4 +1,5 @@
-﻿using reserva.user.backend.DTO.RequestModels;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using reserva.user.backend.DTO.RequestModels;
 using reserva.user.backend.DTO.ResponseModels.Inner;
 using reserva.user.backend.DTO.ResponseModels.Main;
 
@@ -11,5 +12,7 @@ namespace reserva.user.backend.Services.Interface
         Task<ResponseSimple> DeleteAsync(ResponseSimple response, int id);
         Task<CompanyEmployeeVM> GetByIdAsync(int id);
         Task<ResponseListTotal<CompanyEmployeeVM>> GetAll(ResponseListTotal<CompanyEmployeeVM> response, int page, int pageSize);
+        Task<ResponseSimple> UpdateEmployeeAsync(ResponseSimple response, CompanyEmployeeDto model, int id);
+        Task<ResponseSimple> PartiallyUpdateEmployeeAsync(ResponseSimple response, int id, JsonPatchDocument<CompanyEmployeeDto> model);
     }
 }
